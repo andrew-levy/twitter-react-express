e147abd0430874cf8fbf27607d5dd6f915ab44c7
andrew-levy <alevy97@gmail.com>
2019-05-25 15:53:07 -0400

    changed server proxy

--- server.js
+++ server.js
@@ -7,6 +7,20 @@ const app = express();
 const port = process.env.PORT || 5000;
 const post = "";
 
+
+
+var Twit = require('twit')
+
+var T = new Twit({
+
+  consumer_key:         'Qu2IjLLOmjQuA3hF8Lh8bkHFL',
+  consumer_secret:      'yDgvzl7uvjY7lG8qCxvDslKb4kpBqOsLrCwlR9VMpXOn1HOR9y',
+  access_token:         '1131194121836814337-QaDcJYCXaaMuueIej4i1urnz5f2jgY',
+  access_token_secret:  's1Wabivuq0I6VCdT0L48fEW1R0rm3zQ3oP76zuJr5kHjz',
+  
+})
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -20,6 +34,32 @@ app.post('/api/world', (req, res) => {
   res.send(
     `Thanks for tweeting: ${req.body.post}`,
   );
+
+// tweet
+
+
+		// var tweet = { 
+		// 	status:  `${req.body.post}`
+		// }
+
+		// T.post('statuses/update', tweet, tweeted);
+
+
+		// function tweeted (err, data, response) {
+		// 	if(err){
+		// 		console.log('Tweet could not be sent');
+		// 	}
+		//   	else{
+		//   		console.log('Tweet sent');
+		//   	}
+		// }
+
+
+
+
+
+
+
 
   const command = "node reactbot.js " +  `${req.body.post}`;
 
