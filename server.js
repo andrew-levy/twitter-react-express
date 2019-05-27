e6d50ef8bda4d394ad9b0064c316e42def15a387
andrew-levy <alevy97@gmail.com>
2019-05-27 16:48:36 -0400

    updates package and server

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cp = require('child_process');
 
 const app = express();
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
 const post = "";
 
 
